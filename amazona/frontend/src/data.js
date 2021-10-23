425439018b20ff7b589bbfd9348a49ddd122f8a9
Pavankalyan477 <sarakantipavankalyan070@gmail.com>
2021-10-23 17:23:46 +0530

    Ecommerce ReAcT

---
+++ amazona/frontend/src/data.js
@@ -0,0 +1,64 @@
+const data ={
+    products:[
+        {
+            _id:'1',
+            name:'Shirts',
+            image:'/images.p1.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'High quality product'
+        },
+        {
+            _id:'2',
+            name:'adidas fitShirts',
+            image:'/images/p2.jpg',
+            price:100,
+            brand:'Adidas',
+            rating:4,
+            numReviews:10,
+            description:'High quality product'
+        },
+        {
+            _id:'3',
+            name:'Lack Shirts',
+            image:'/images/p3.jpg',
+            price:1200,
+            brand:'Lacoste',
+            rating:4.7,
+            numReviews:10,
+            description:'High quality product'
+        },
+        {
+            _id:'4',
+            name:'Nike pants',
+            image:'/images/p4.jpg',
+            price:190,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'High quality product'
+        },
+        {
+            _id:'5',
+            name:'puma slim Shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Puma',
+            rating:4.5,
+            numReviews:10,
+            description:'High quality product'
+        },
+        {
+            _id:'6',
+            name:'adidas slim pantss',
+            image:'/images/p2.jpg',
+            price:120,
+            brand:'Adidas',
+            rating:4.5,
+            numReviews:10,
+            description:'High quality product'
+        }
+    ]
+}
